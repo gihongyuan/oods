ac99fec06a6b8da5541e1621e762ff66d0e27bb1
陈洪元 <chenhy@cyberpeace.cn>
2023-08-30 10:07:18 +0800

    feature: 关闭生产模式的DEBUG

--- oods/oods/settings.py
+++ oods/oods/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-if(_zgid06rdr2+05-j7xb@i-lyn22!f!scz89p(&!w&^xpv)2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 X_FRAME_OPTIONS = 'ALLOWALL'
 
